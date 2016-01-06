2c1f79fd087707febaf4d6e733f43a650a0603e4
yupi2 <yupi2@openmailbox.org>
2016-01-06 03:38:34 -0600

    README.md update & small message changes

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-This uses vON to serialize the table to a string then it just uses GMOD's compression functions.
+Used to compress tables sent over the [net-messaging system](http://wiki.garrysmod.com/page/Category:net) to lessen the strain on servers and clients sending large amounts of data.
 
-See
-https://github.com/vercas/vON/
-or
+This is done by overwriting [net.WriteTable](http://wiki.garrysmod.com/page/net/WriteTable) & [net.ReadTable](http://wiki.garrysmod.com/page/net/ReadTable) to compress or decompress the table.
+
+See the **Accessible functions** section below to use either the vanilla or compressing versions explicitly.
+
+Requirements
+============
+
+https://github.com/vercas/vON
+
+**OR**
+
 https://github.com/yupi2/vON
+
+Accessible functions
+====================
+
+- net.OrigWriteTable       - The original net.WriteTable function
+- net.OrigReadTable        - The original net.ReadTable function
+- net.WriteCompressedTable - The net.WriteTable version that compresses
+- net.ReadCompressedTable  - The net.ReadTable version that decompresses
--- lua/autorun/net_compressed_table.lua
+++ lua/autorun/net_compressed_table.lua
@@ -2,27 +2,27 @@ local net = net
 
 if von == nil then
 	local vonfile = "autorun/von.lua"
-	local errorstr = "vON (GMOD Lua Table serialization) is not available!" ..
-		" Download https://github.com/vercas/vON/blob/master/von.lua and" ..
-		" put it as garrysmod/lua/autorun/von.lua"
+	local errorstr = [[vON (GMOD Lua Table serialization) is not available!
+Download https://github.com/vercas/vON/blob/master/von.lua and
+save it as garrysmod/lua/autorun/von.lua]]
 
 	if file.Exists(vonfile, "LUA") then
 		if SERVER then AddCSLuaFile(vonfile) end
 		include(vonfile)
 	else
-		-- assert + print for emphasis!
+		-- assert + print for emphasis! EDIT: why...
 		print(errorstr)
-		assert(von ~= nil, errorstr)
+		assert(false, errorstr)
 	end
 end
 
 net.OrigWriteTable = net.OrigWriteTable or net.WriteTable
 net.OrigReadTable = net.OrigReadTable or net.ReadTable
 
--- Read compressed tables.
 function net.ReadCompressedTable()
 	local size = net.ReadUInt(32)
 
+	-- size of zero indicates an empty table
 	if size == 0 then
 		return {}
 	end
@@ -30,10 +30,8 @@ function net.ReadCompressedTable()
 	return von.deserialize(util.Decompress(net.ReadData(size)))
 end
 
--- Write compressed tables.
 function net.WriteCompressedTable(tbl)
-	-- is table empty
-	if next(tbl) == nil then
+	if next(tbl) == nil then -- is table empty
 		-- send a size of zero to indicate an empty table
 		net.WriteUInt(0, 32)
 	else
